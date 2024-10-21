3f2859662f59df4a572fa6df1cb6c521f9e1e0d7
caiofraporti <caiosk8er@gmail.com>
2024-10-21 13:13:53 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Ola mundao");
+let name = "Caio"
+let qtdeXp = 500
+let nivel = ""
+
+switch(true){
+    case qtdeXp <= 1000:
+        nivel = "Ferro";
+        break;
+    case qtdeXp >= 1001 && qtdeXp <= 2000:
+        nivel = "Bronze";
+        break;
+    case qtdeXp >= 2001 && qtdeXp <= 5000:
+        nivel = "Prata";
+        break;
+    case qtdeXp >= 5001 && qtdeXp <= 7000:
+        nivel = "Ouro";
+        break;
+    case qtdeXp >= 7001 && qtdeXp <= 8000:
+        nivel = "Platina";
+        break;
+    case qtdeXp >= 8001 && qtdeXp <= 9000:
+        nivel = "Ascendente";
+        break;
+    case qtdeXp >= 9001 && qtdeXp <= 10000:
+        nivel = "Imortal";
+        break;
+    default:
+        nivel = "Radiante";
+        break;
+}
+
+console.log("O heroi de nome", name, "está no nivel", nivel);
